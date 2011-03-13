052b0c89400c9f2f7275ddf147f1de5d42543394
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-03-13 20:41:45 +0100

    Translated some comments

--- src/Blockbuster/DirectoryIterator/ObservableFileAndDirectoriyIterator.cs
+++ src/Blockbuster/DirectoryIterator/ObservableFileAndDirectoriyIterator.cs
@@ -7,36 +7,37 @@ using Blockbuster.CommonTypes;
 
 namespace Blockbuster.DirectoryIterator
 {
-    public class ObservableFileAndDirectoryIterator : IFileSystemIterator
-    {
-        public IObservable<FileSystemEntity> Iterate(string directoryFullName)
-        {
-            DirectoryInfo entry = new DirectoryInfo(directoryFullName);
+	public class ObservableFileAndDirectoryIterator : IFileSystemIterator
+	{
+		public IObservable<FileSystemEntity> Iterate(string directoryFullName)
+		{
+			DirectoryInfo entry = new DirectoryInfo(directoryFullName);
 
-            //Frischen Datenstrom von FileSystemEntities erstellen
-            var observableFileSystemEntities = Observable.Empty<FileSystemEntity>();
+            //Create a new data stream of FileSystemEntities (becoming our core data stream)
+			var observableFileSystemEntities = Observable.Empty<FileSystemEntity>();
 
-            //Datenstrom von DirectoryInfos ausgehend vom Root Directory erstellen
+			//Create a new data stream of DirectoryInfos from the command root directory
             var observableDirectories = entry.EnumerateDirectories("*", SearchOption.AllDirectories).ToObservable();
 
-            //Datenstrom aus Dateien im TopLevelDirectory erstellen
-            var observableFirstLevelFiles = entry.EnumerateFiles("*", SearchOption.TopDirectoryOnly)
-                                                .ToObservable()
-                                                .Select(x => new FileSystemEntity(x));
+            //Create a new data stream of FileSystemEntities based on the files laying inside the command root top level directory 
+			var observableFirstLevelFiles = entry.EnumerateFiles("*", SearchOption.TopDirectoryOnly)
+												.ToObservable()
+												.Select(x => new FileSystemEntity(x));
 
-            //Datenstrom der TopLevel Files in den HauptDatenstrom mergen
-            observableFileSystemEntities = observableFileSystemEntities.Merge(observableFirstLevelFiles);
+			//merge previous data stream into our core data stream of FileSystemEntities
+			observableFileSystemEntities = observableFileSystemEntities.Merge(observableFirstLevelFiles);
 
-            //Datenstrom von FileSystemEntities auf Basis der DirectoryInfos in den Hauptdatenstrom
-            //von FileSystemEntities mergen
-            observableFileSystemEntities = observableFileSystemEntities.Merge(observableDirectories.Select(x => new FileSystemEntity(x)));
+            //Based on our stream of DirectoryInfos, project it into a stream of FileSystemEntities and merge it
+            //into our core data stream
+			observableFileSystemEntities = observableFileSystemEntities.Merge(observableDirectories.Select(x => new FileSystemEntity(x)));
 
-            //Datenstrom von FileSystemEntities auf Basis der FileInfos aller Directories
-            //erstellen und in den Hauptdatenstrom von FileSystemEntities mergen
-            var observableFiles = observableDirectories.SelectMany(x => x.EnumerateFiles().ToObservable()).Select(x => new FileSystemEntity(x));
-            observableFileSystemEntities = observableFileSystemEntities.Merge(observableFiles);
+            //Based on our stream of DirectoryInfos, project each stream of FileInfos into a flat stream
+            //of FileInfos, project this into a stream of FileSystemEntities and merge it into our core stream
+			var observableFiles = observableDirectories.SelectMany(x => x.EnumerateFiles().ToObservable()).Select(x => new FileSystemEntity(x));
+			observableFileSystemEntities = observableFileSystemEntities.Merge(observableFiles);
 
-            return observableFileSystemEntities;
-        }
-    }
+            //return our core data stream
+			return observableFileSystemEntities;
+		}
+	}
 }
