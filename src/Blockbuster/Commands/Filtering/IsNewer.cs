4fa26d1411943c3c0082f210cf38e83c5ae7fd3d
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-03-15 23:34:42 +0100

    Added IsNewer, IsOlder Commands

---
+++ src/Blockbuster/Commands/Filtering/IsNewer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blockbuster.Contracts;
+
+namespace Blockbuster.Commands.Filtering
+{
+    public class IsNewer : AbstractCommand
+    {
+        DateTime _dateTime;
+
+        public IsNewer(DateTime dateTime) 
+        {
+            _dateTime = dateTime;
+        }
+
+        public override IObservable<FileSystemEntity> FilterFileSystemEntities(IObservable<FileSystemEntity> source)
+        {
+            return source.Where(x => x.CreationTime > _dateTime);
+        }
+
+        public override string Name
+        {
+            get { return "IsNewer"; }
+        }
+    }
+}
