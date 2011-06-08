71aa23dd7dc1030f7910116af1ea25840a98f97a
Christoph Burgdorf <christoph.burgdorf@dff-solutions.de>
2011-06-08 13:44:23 +0200

    added "KeepLastHour" command

---
+++ src/Blockbuster/Commands/Filtering/KeepLastHour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blockbuster.Contracts;
+
+namespace Blockbuster.Commands.Filtering
+{
+    public class KeepLastHour : AbstractCommand
+    {
+        public override IObservable<FileSystemEntity> FilterFileSystemEntities(IObservable<FileSystemEntity> source)
+        {
+            return source.Where(x => x.CreationTime < DateTime.Now.AddHours(-1));
+        }
+
+        public override string Name
+        {
+            get { return "KeepLastHour"; }
+        }
+    }
+}
