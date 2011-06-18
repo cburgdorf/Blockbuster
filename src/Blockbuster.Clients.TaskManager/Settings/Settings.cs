f992b1f86227516e204c93319a55d02521500126
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-06-18 21:05:12 +0200

    New Configuration API coming along nicely along with a bunch of unit tests

---
+++ src/Blockbuster.Clients.TaskManager/Settings/Settings.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace Blockbuster.Clients.TaskManager.Settings
+{
+    public class Settings
+    {
+        public Settings()
+        {
+            _commandConfigurations = new List<CommandConfiguration>();
+        }
+
+        private readonly List<CommandConfiguration> _commandConfigurations;
+
+        public IEnumerable<CommandConfiguration> CommandConfigurations
+        {
+            get { return _commandConfigurations; }
+        }
+
+        public static Settings FromAppConfig()
+        {
+            var section = (NameValueCollection)ConfigurationManager.GetSection("CleanUpTasks");
+
+            var settings = new Settings();
+
+            for (int i = 0; i < section.Keys.Count; i++)
+            {
+                string commandName = section.Keys[i];
+                var values = section.GetValues(commandName);
+                if (values == null || values.Length == 0)
+                    continue;
+
+                string value = values[0];
+
+                var splittedValue = value.Split('|');
+
+                if (splittedValue.Length < 2)
+                    continue;
+
+                settings._commandConfigurations.Add(new CommandConfiguration
+                                                        {
+                                                            EntryName = commandName,
+                                                            CommandName = splittedValue[0],
+                                                            DirectoryPath = splittedValue[1]
+                                                        });
+            }
+            return settings;
+        }
+    }
+
+    public class CommandConfiguration
+    {
+        public string EntryName { get; set; }
+        public string CommandName { get; set; }
+        public string DirectoryPath { get; set; }
+    }
+}
---
+++ src/Blockbuster/Configuration/CommandConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blockbuster.Configuration
+{
+    public class CommandConfiguration
+    {
+        public CommandConfiguration(string configurationString)
+        {
+            Configuration = ParseString(configurationString);
+        }
+
+        /// <summary>
+        /// Splits up an configuration string and returns an Dictionary<string, string>
+        /// (e.g. "CommandName='FilesOnly'; Directory='C:\\Test\\'" becomes 
+        /// new Dictionary<string, string>
+        /// {
+        ///     {"commandname", "filesonly"},
+        ///     {"directory", "c:\\test\\"}
+        /// })
+        /// </summary>
+        private Dictionary<string, string> ParseString(string configurationString)
+        {
+            Func<string, string> sanitizeString = x => x.Trim().ToLower().Replace("'", "");
+
+            return configurationString
+                .Split(';')
+                .Select(x => x.Split('='))
+                .Where(x => x.Length == 2)
+                .ToDictionary(x => sanitizeString(x[0]), x => sanitizeString(x[1]));
+        }
+
+        public Dictionary<string, string> Configuration { get; protected set; }
+    }
+}
