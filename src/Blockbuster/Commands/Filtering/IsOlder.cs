b80f9fc78cf06e08e5bae8b87ae7ed3a4622a7f4
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-07-26 09:07:46 +0200

    - Quick hack to let the IsOlder Command to be configureable with relative datetime (days, minutes, hours) via Dynamic Configuration (e.g. XML) - updated binaries

--- src/Blockbuster/Commands/Filtering/IsOlder.cs
+++ src/Blockbuster/Commands/Filtering/IsOlder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Blockbuster.Contracts;
+using Blockbuster.Extensions;
 
 namespace Blockbuster.Commands.Filtering
 {
@@ -23,6 +24,27 @@ namespace Blockbuster.Commands.Filtering
         {
             if (value.ContainsKey("isolder"))
                 _dateTime = DateTime.Parse(value["isolder"]);
+			else
+            {
+            	var olderThan = DateTime.Now;
+				if (value.ContainsKey("olderminutes"))
+				{
+					var minutesFromNow = value["olderminutes"].ParseToNullableDouble();
+					_dateTime = minutesFromNow.HasValue ? olderThan.AddMinutes(minutesFromNow.Value * -1) : olderThan;
+				}
+				if (value.ContainsKey("olderhours"))
+				{
+					var hoursFromNow = value["olderhours"].ParseToNullableDouble();
+					_dateTime = hoursFromNow.HasValue ? olderThan.AddHours(hoursFromNow.Value * -1) : olderThan;
+				}
+				if (value.ContainsKey("olderdays"))
+				{
+					var daysFromNow = value["olderdays"].ParseToNullableDouble();
+					_dateTime = daysFromNow.HasValue ? olderThan.AddDays(daysFromNow.Value * -1) : olderThan;
+				}
+            }
+			
+				
         }
 
         public override IObservable<FileSystemEntity> FilterFileSystemEntities(IObservable<FileSystemEntity> source)
---
+++ src/Blockbuster/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blockbuster.Extensions
+{
+	public static class StringExtensions
+	{
+		public static DateTime? SubstractMinutesFromNow(this string str)
+		{
+			double minutes = 0;
+			if (double.TryParse(str, out minutes))
+			{
+				return DateTime.Now.AddMinutes(minutes * -1);
+			}
+			return null;
+		}
+
+		public static double? ParseToNullableDouble(this string str)
+		{
+			double result;
+			if (double.TryParse(str, out result))
+			{
+				return result;
+			}
+			return null;
+		}
+	}
+}
